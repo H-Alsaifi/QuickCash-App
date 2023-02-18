2a391f1c1eed15f009fda371844ca9bc4c52089d
Adja Niang <Adja.Niang@dal.ca>
2023-02-18 18:35:16 -0400

    user_profile

--- app/src/main/java/com/example/g2_qc/user_profile/userDetails.java
+++ app/src/main/java/com/example/g2_qc/user_profile/userDetails.java
@@ -3,6 +3,8 @@ package com.example.g2_qc.user_profile;
 public class userDetails {
     public String age, phoneNumber, gender;
 
+    public userDetails() {}
+
     public userDetails(String user_age, String user_phoneNumber, String user_gender) {
         this.age = user_age;
         this.phoneNumber = user_phoneNumber;
