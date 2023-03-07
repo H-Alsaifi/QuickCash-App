d8b1827b7cc16bfc676d938a4c0fb7f0940c315a
mohammed <mshalqassabi@gmail.com>
2023-03-07 03:05:48 -0400

    "added a java class 'SubmitJob', also a layout for it 'add_job' with a field to write the name of the job that the user wants to add, and a bigger field to describe the job, I also added another field to write the job payment per hour"

---
+++ app/src/main/java/com/example/g2_qc/submitNewJob/SubmitJob.java
@@ -0,0 +1,16 @@
+package com.example.g2_qc.submitNewJob;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.g2_qc.R;
+
+public class SubmitJob extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_job);
+    }
+}
