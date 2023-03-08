117ae4db77e10ef597056353c31c711f0b36d154
mohammed <mshalqassabi@gmail.com>
2023-03-08 19:44:31 -0400

    "added a field to upload an image, a spinner to go through the available categories and to choose one, and a button to submit the job ad."

--- app/src/main/java/com/example/g2_qc/submitNewJob/SubmitJob.java
+++ app/src/main/java/com/example/g2_qc/submitNewJob/SubmitJob.java
@@ -1,16 +1,142 @@
 package com.example.g2_qc.submitNewJob;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.g2_qc.MainActivity;
 import com.example.g2_qc.R;
+import com.example.g2_qc.signup_page.User;
+import com.example.g2_qc.signup_page.signup;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Locale;
+import java.util.UUID;
+import java.text.NumberFormat;
+
+
 
 public class SubmitJob extends AppCompatActivity {
+
+    private EditText jobName;
+    private EditText jobDescription;
+    private EditText jobPayment;
+    private ImageView jobImage;
+    private Button submitJobButton;
+    private Spinner categoriesSpinner;
+    private static final int REQUEST_CODE_OPEN_DOCUMENT = 1;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_job);
+
+        jobName = findViewById(R.id.job_name);
+        jobDescription = findViewById(R.id.job_description);
+        jobPayment = findViewById(R.id.job_payment);
+        categoriesSpinner = findViewById(R.id.categories_spinner);
+        jobImage = findViewById(R.id.jobImage);
+        submitJobButton = findViewById(R.id.submit_job);
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.JobsCategories, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categoriesSpinner.setAdapter(adapter);
+        jobImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), REQUEST_CODE_OPEN_DOCUMENT);
+            }
+        });
+
+        submitJobButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = jobName.getText().toString();
+                String description = jobDescription.getText().toString();
+                String paymentStr = jobPayment.getText().toString();
+                String category = categoriesSpinner.getSelectedItem().toString();
+
+                if (name.isEmpty()) {
+                    jobName.setError("Please enter a job name");
+                    jobName.requestFocus();
+                    return;
+                }
+
+                else if (description.isEmpty()) {
+                    jobDescription.setError("Please enter a job description");
+                    jobDescription.requestFocus();
+                    return;
+                }
+
+                else if (paymentStr.isEmpty()) {
+                    jobPayment.setError("Please enter a job payment");
+                    jobPayment.requestFocus();
+                    return;
+                }
+
+
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE_OPEN_DOCUMENT && resultCode == RESULT_OK) {
+            Uri selectedImageUri = data.getData();
+
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference();
+            StorageReference imagesRef = storageRef.child("images/" + selectedImageUri.getLastPathSegment());
+
+            UploadTask uploadTask = imagesRef.putFile(selectedImageUri);
+
+            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Toast.makeText(SubmitJob.this, "Image uploaded successfully", Toast.LENGTH_SHORT).show();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    Toast.makeText(SubmitJob.this, "Image upload failed", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+
 }
