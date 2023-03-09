2054378c1210159e7112990f2d0672091138bab6
mohammed <mshalqassabi@gmail.com>
2023-03-09 01:21:11 -0400

    "I changed the layout of SubmitJob.java, I made them two classes, one for employee and the other one for employer, in order to allow both of them to have separate sections on the database so the data can be easily retrieved, I also enabled error messages to pop up when the fields are empty ,and not allow the user to submit the ad. I also added java class for posts (Post.java).

--- app/src/main/java/com/example/g2_qc/submitNewJob/SubmitJobAsEmployee.java
+++ app/src/main/java/com/example/g2_qc/submitNewJob/SubmitJobAsEmployee.java
@@ -2,7 +2,6 @@ package com.example.g2_qc.submitNewJob;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -11,42 +10,20 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
-
-import androidx.activity.result.ActivityResultCallback;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.example.g2_qc.MainActivity;
 import com.example.g2_qc.R;
-import com.example.g2_qc.signup_page.User;
-import com.example.g2_qc.signup_page.signup;
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.Locale;
-import java.util.UUID;
-import java.text.NumberFormat;
 
-
-
-public class SubmitJob extends AppCompatActivity {
+public class SubmitJobAsEmployee extends AppCompatActivity {
 
     private EditText jobName;
     private EditText jobDescription;
@@ -54,19 +31,20 @@ public class SubmitJob extends AppCompatActivity {
     private ImageView jobImage;
     private Button submitJobButton;
     private Spinner categoriesSpinner;
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference("Users");
     private static final int REQUEST_CODE_OPEN_DOCUMENT = 1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_job);
+        setContentView(R.layout.add_job_as_employee);
 
-        jobName = findViewById(R.id.job_name);
-        jobDescription = findViewById(R.id.job_description);
-        jobPayment = findViewById(R.id.job_payment);
-        categoriesSpinner = findViewById(R.id.categories_spinner);
-        jobImage = findViewById(R.id.jobImage);
-        submitJobButton = findViewById(R.id.submit_job);
+        jobName = findViewById(R.id.job_name_employee);
+        jobDescription = findViewById(R.id.job_description_employee);
+        jobPayment = findViewById(R.id.job_payment_employee);
+        categoriesSpinner = findViewById(R.id.categories_spinner_employee);
+        jobImage = findViewById(R.id.jobImage_employee);
+        submitJobButton = findViewById(R.id.submit_job_employee);
 
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.JobsCategories, android.R.layout.simple_spinner_item);
@@ -92,22 +70,19 @@ public class SubmitJob extends AppCompatActivity {
                 if (name.isEmpty()) {
                     jobName.setError("Please enter a job name");
                     jobName.requestFocus();
-                    return;
                 }
 
-                else if (description.isEmpty()) {
+                if (description.isEmpty()) {
                     jobDescription.setError("Please enter a job description");
                     jobDescription.requestFocus();
-                    return;
                 }
 
-                else if (paymentStr.isEmpty()) {
+                if (paymentStr.isEmpty()) {
                     jobPayment.setError("Please enter a job payment");
                     jobPayment.requestFocus();
-                    return;
                 }
 
-
+                Toast.makeText(SubmitJobAsEmployee.this, "Please Select Image", Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -121,19 +96,51 @@ public class SubmitJob extends AppCompatActivity {
 
             FirebaseStorage storage = FirebaseStorage.getInstance();
             StorageReference storageRef = storage.getReference();
-            StorageReference imagesRef = storageRef.child("images/" + selectedImageUri.getLastPathSegment());
+            StorageReference imagesRef = storageRef.child("images/"
+                    + selectedImageUri.getLastPathSegment());
 
-            UploadTask uploadTask = imagesRef.putFile(selectedImageUri);
+            jobImage.setImageURI(selectedImageUri);
 
-            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            submitJobButton.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    Toast.makeText(SubmitJob.this, "Image uploaded successfully", Toast.LENGTH_SHORT).show();
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception exception) {
-                    Toast.makeText(SubmitJob.this, "Image upload failed", Toast.LENGTH_SHORT).show();
+                public void onClick(View v) {
+                    String name = jobName.getText().toString();
+                    String description = jobDescription.getText().toString();
+                    String paymentStr = jobPayment.getText().toString();
+                    String category = categoriesSpinner.getSelectedItem().toString();
+
+                    if (name.isEmpty()) {
+                        jobName.setError("Please enter a job name");
+                        jobName.requestFocus();
+                        return;
+                    }
+
+                    else if (description.isEmpty()) {
+                        jobDescription.setError("Please enter a job description");
+                        jobDescription.requestFocus();
+                        return;
+                    }
+
+                    else if (paymentStr.isEmpty()) {
+                        jobPayment.setError("Please enter a job payment");
+                        jobPayment.requestFocus();
+                        return;
+                    }
+
+                    Post post = new Post(name, description, paymentStr, selectedImageUri.toString(), category);
+
+                    String postID = root.push().getKey();
+
+                    FirebaseDatabase.getInstance().getReference("Users")
+                            .child("SRKWLMPkIxeSzH9AOkhfMTNEvbP2")
+                            .child("Employee").child("Posts").child(postID)
+                            .setValue(post).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    Toast.makeText(SubmitJobAsEmployee.this, "Job Posted", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                    UploadTask uploadTask = imagesRef.putFile(selectedImageUri);
                 }
             });
         }
