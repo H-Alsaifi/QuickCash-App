d8f6bfb147b0b57a00914b9fa804bbf5685125a4
jaitly <ds389363@dal.ca>
2023-03-16 19:57:49 -0300

    Job display feature added

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Group 2 - Quick Cash
 
 
-
+ls
 ## Getting started
 
 To make it easy for you to get started with GitLab, here's a list of recommended next steps.
---
+++ app/src/main/java/com/example/g2_qc/display_details/display_details.java
@@ -0,0 +1,89 @@
+package com.example.g2_qc.display_details;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.example.g2_qc.R;
+import com.example.g2_qc.main_page.MainPageActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class display_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_details);
+
+        String myId = FirebaseAuth.getInstance().getCurrentUser().getUid(); //user ID
+        extractInfo("-NQckYWwbc3Hg_vjdtQm", "wFZqWpbGHQMoCWNxJala3ibWs523");
+//        FirebaseDatabase db = FirebaseDatabase.getInstance("https://quickcash-group2-default-rtdb.firebaseio.com/");
+//        DatabaseReference ref = db.getReference().child("users");
+
+
+
+
+
+    }
+    public void extractInfo(String postId, String UserUID) {
+        DatabaseReference databaseReferenceEmployee = FirebaseDatabase.getInstance().getReference()
+                .child("Users").child(UserUID).child("Employee").child("Posts").getRef();
+        databaseReferenceEmployee.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                extractPostInfo(dataSnapshot, postId);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(display_details.this, "Error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        DatabaseReference databaseReferenceEmployer = FirebaseDatabase.getInstance().getReference()
+                .child("Users").child(UserUID).child("Employee").child("Posts").getRef();
+        databaseReferenceEmployer.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                extractPostInfo(dataSnapshot, postId);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(display_details.this, "Error: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+
+    public void extractPostInfo(DataSnapshot dataSnapshot, String postId) {
+        for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+            if (postId.equals(postSnapshot.getKey())) {
+                String jobName = postSnapshot.child("jobName").getValue(String.class);
+                String timePosted = postSnapshot.child("timePosted").getValue(String.class);
+                String jobCategory = postSnapshot.child("jobCategory").getValue(String.class);
+                String description = postSnapshot.child("jobDescription").getValue(String.class);
+                String payment = postSnapshot.child("jobPayment").getValue(String.class);
+                //any other info
+
+                TextView view = findViewById(R.id.display);
+                view.setText("Job Name: " + jobName + "\nJob category: " + jobCategory + "\nJob description: " + description + "\nPayment: " + payment + "\nTime Posted: " + timePosted);
+
+
+            }
+
+        }
+
+    }
+
+}
