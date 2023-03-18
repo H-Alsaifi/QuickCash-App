ecb48a9d2083b555d5edc7465ca8131b40270d5f
mohammed <mshalqassabi@gmail.com>
2023-03-18 00:20:03 -0300

    - greetingPage.java: Instead of getting the users first name from the Authentications I designed it to retrieve it from the FirebaseDatabase. - Post.java: Deleted a useless constructor.

--- app/src/main/java/com/example/g2_qc/greeting_page/greetingPage.java
+++ app/src/main/java/com/example/g2_qc/greeting_page/greetingPage.java
@@ -6,22 +6,31 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import com.example.g2_qc.R;
 import com.example.g2_qc.main_page.MainPageActivity;
 
+import com.example.g2_qc.user_profile.userDetails;
+import com.example.g2_qc.user_profile.userProfile;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class greetingPage extends AppCompatActivity {
 
     private TextView welcomeUserText;
     private TextView greetText;
     private Button getStarted;
-    private FirebaseAuth firebaseAuth;
+    private DatabaseReference root = FirebaseDatabase.getInstance().getReference("Users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +40,22 @@ public class greetingPage extends AppCompatActivity {
         greetText = findViewById(R.id.greetingText);
         welcomeUserText = findViewById(R.id.welcomeUser);
         getStarted = findViewById(R.id.getStartedButtom);
-        firebaseAuth = FirebaseAuth.getInstance();
+        root = FirebaseDatabase.getInstance().getReference("Users");
+
+        root.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                userDetails userDetails = snapshot.getValue(userDetails.class);
+                if(userDetails != null){
+                    String fName = userDetails.firstName;
+                    welcomeUserText.setText("Welcome, " + fName + "!");
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(greetingPage.this, "You are welcome whomever you are!", Toast.LENGTH_LONG).show();
+            }
+        });
 
 
         getStarted.setOnClickListener(new View.OnClickListener() {
@@ -41,11 +65,7 @@ public class greetingPage extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-        if (user != null) {
-            String firstName = firebaseAuth.getCurrentUser().getDisplayName();
-            welcomeUserText.setText("Welcome, " + firstName + "!");
-        }
+
     }
 }
 
