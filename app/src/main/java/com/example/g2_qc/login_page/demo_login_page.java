e6a1d2a087582d9a073febf1ed2a5714e0ec580f
Adja Niang <Adja.Niang@dal.ca>
2023-04-06 00:31:52 -0300

    paypal

--- app/src/main/java/com/example/g2_qc/login_page/demo_login_page.java
+++ app/src/main/java/com/example/g2_qc/login_page/demo_login_page.java
@@ -34,12 +34,6 @@ public class demo_login_page extends AppCompatActivity {
 
         etEmail = findViewById(R.id.et_email_address);
         etPassword = findViewById(R.id.et_password);
-
-        // for debug mode
-        etEmail.setText("uhruvb@gmail.com");
-        etPassword.setText("Adjita97!");
-
-
         btnLogin = findViewById(R.id.btn_login);
         tvForgotPassword = findViewById(R.id.tv_forgot_password);
         tvForgotPassword.setPaintFlags(tvForgotPassword.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
